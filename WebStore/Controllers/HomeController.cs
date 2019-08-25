ac3922d4284bd50de008366fab894d11673004cc
ice19942335 <ice19942335@gmail.com>
2019-08-25 11:02:25 +0100

    Added main pages in project + configuration of _Latout + _LayoutBase

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -8,9 +8,24 @@ namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        public IActionResult Index() => RedirectToAction(nameof(Shop));
+
+        public IActionResult Shop() => View();
+
+        public IActionResult ProductDetails() => View();
+
+        public IActionResult Login() => View();
+
+        public IActionResult ContactUs() => View();
+
+        public IActionResult CheckOut() => View();
+
+        public IActionResult Cart() => View();
+
+        public IActionResult BlogSingle() => View();
+
+        public IActionResult Blog() => View();
+
+        public IActionResult NotFound() => View();
     }
 }
