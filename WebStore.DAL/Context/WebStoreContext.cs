d20118d0413fc283f08ef464e7fb626ccf0db034
ice19942335 <ice19942335@gmail.com>
2019-08-30 18:34:00 +0100

    Added DB_Context EF + Employee entity

---
+++ WebStore.DAL/Context/WebStoreContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using WebStore.Domain.Entities;
+
+namespace WebStore.DAL.Context
+{
+    public class WebStoreContext : DbContext
+    {
+        public  DbSet<Product> Products { get; set; }
+
+        public  DbSet<Section> Sections { get; set; }
+
+        public  DbSet<Brand> Brands { get; set; }
+
+        public WebStoreContext(DbContextOptions<WebStoreContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
---
+++ WebStore.Domain/WebStore.Domain/Entities/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using WebStore.Domain.Entities.Base;
+
+namespace WebStore.Domain.Entities
+{
+    public class Employee : BaseEntity
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        public string Patronymic { get; set; }
+
+        [Required]
+        public string Surname { get; set; }
+
+        [Required]
+        public int Age { get; set; }
+    }
+}
