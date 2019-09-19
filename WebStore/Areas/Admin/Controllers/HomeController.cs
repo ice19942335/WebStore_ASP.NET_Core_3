38cb08455a2526414be5e564542bf0e8ea10ad73
ice19942335 <ice19942335@gmail.com>
2019-09-19 12:30:52 +0100

    Area "Admin" + _LayoutBase + _Layout + ProductList page in Admin area

--- WebStore/Areas/Admin/Controllers/HomeController.cs
+++ WebStore/Areas/Admin/Controllers/HomeController.cs
@@ -1,17 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WebStore._Infrastructure.Interfaces;
+using WebStore.Domain.Entities;
 
 namespace WebStore.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = Domain.Entities.Identity.User.RoleAdmin)]
     public class HomeController : Controller
     {
+        private readonly IProductData _productData;
+        public HomeController(IProductData productData) => _productData = productData;
+
         public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult ProductList() => View(_productData.GetProducts(new ProductFilter()));
     }
 }
