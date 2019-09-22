b297d5c0b7cc0c486ac5a91d7cb1f3c62788d5f0
ice19942335 <ice19942335@gmail.com>
2019-09-22 17:34:24 +0100

    Client WebAPI - IProductData

---
+++ Services/WebStore.Clients/Products/ProductsClient.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using WebStore.Clients.Base;
+using WebStore.Domain.DTO.Product;
+using WebStore.Domain.Entities.Product;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Clients.Products
+{
+    class ProductsClient : BaseClient, IProductData
+    {
+        public ProductsClient(IConfiguration configuration) : base(configuration, "api/products")
+        {
+        }
+
+        public IEnumerable<Section> GetSections()
+        {
+            return Get<List<Section>>($"{_serviceAddress}/sections");
+        }
+
+        public IEnumerable<Brand> GetBrands()
+        {
+            return Get<List<Brand>>($"{_serviceAddress}/brands");
+        }
+
+        public IEnumerable<ProductDTO> GetProducts(ProductFilter filter)
+        {
+            var response = Post(_serviceAddress, filter);
+            return response.Content.ReadAsAsync<IEnumerable<ProductDTO>>().Result;
+        }
+
+        public ProductDTO GetProductById(int id)
+        {
+            return Get<ProductDTO>($"{_serviceAddress}/{id}");
+        }
+    }
+}
